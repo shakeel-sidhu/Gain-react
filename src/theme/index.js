58dd8a9e7cc65c327445900aae60c9c4878ca6e8
Shakeel Ahmed <shakeel.ahmed@starzplayarabia.com>
2023-07-27 16:07:17 +0500

    minor change

--- src/theme/index.js
+++ src/theme/index.js
@@ -7,6 +7,26 @@ import CoinbaseTextMedium  from "../../public/resources/fonts/CoinbaseText-Mediu
 
 // Create a theme instance.
 const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#556cd6',
+		},
+		secondary: {
+			main: '#19857b',
+		},
+		error: {
+			main: red.A400,
+		},
+	},
+	typography:{
+		fontFamily: [
+		  "CoinbaseTextMedium",
+		  'sans-serif'
+		].join(','),
+		fontFamily: [
+			"Coinbase Sans"
+	   ].join(','),
+	},
 	components: {
 		MuiCssBaseline: {
 		  styleOverrides: `
@@ -32,51 +52,6 @@ const theme = createTheme({
 			`,
 		  },
 	  },
-	palette: {
-		primary: {
-			main: '#556cd6',
-		},
-		secondary: {
-			main: '#19857b',
-		},
-		error: {
-			main: red.A400,
-		},
-	},
-	typography:{
-		fontFamily: [
-		  "CoinbaseTextMedium",
-		  'sans-serif'
-		].join(','),
-		fontFamily: [
-			"Coinbase Sans"
-	   ].join(','),
-	},
-	// components: {
-	// 	MuiCssBaseline: {
-	// 	  styleOverrides: `
-	// 		@font-face {
-	// 			font-family: 'Coinbase Sans';
-	// 			src: url(${CoinbaseWoff2}) format('woff2'),
-	// 				url(${CoinbaseWoff}) format('woff');
-	// 			font-weight: 400;
-	// 			font-style: normal;
-	// 			font-display: swap;
-	// 		}
-	// 	  `,
-	// 	},
-	// 	MuiCssBaseline: {
-	// 		styleOverrides: `
-	// 		  @font-face {
-	// 			font-family: 'CoinbaseTextMedium';
-	// 			font-style: normal;
-	// 			font-display: swap;
-	// 			font-weight: 300;
-	// 			src:  url(${CoinbaseTextMedium}) format('truetype');
-	// 		  }
-	// 		`,
-	// 	  },
-	//   },
 })
 
 export default theme
